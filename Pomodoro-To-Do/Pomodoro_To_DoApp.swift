70b531c4ecd904075b00e6514c2e90f64463fe70
bnrdelk <bnrdelk@icloud.com>
2023-03-12 13:11:34 +0300

    Initial Commit

---
+++ Pomodoro-To-Do/Pomodoro_To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_To_DoApp.swift
+//  Pomodoro-To-Do
+//
+//  Created by Beyza Nur Deliktaş on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
